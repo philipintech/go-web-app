7824a79ee391c3f468631a0a5e8810d56a0e37ed
Philip Cheung <philipintech@gmail.com>
2022-10-10 15:13:16 -0400

    first lesson

--- learning-go/main.go
+++ learning-go/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
+	var whatToSay string
+	var i int = 8
+	whatToSay = "Goodbye world"
+	fmt.Println(whatToSay)
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, whenWasSaid := saySomething()
+	fmt.Println("The function returned", whatWasSaid, whenWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "I am saying something", "now"
 }
