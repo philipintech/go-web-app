f662bb34b0068acc42c0a4e484a865ac53499b46
Philip Cheung <philipintech@gmail.com>
2022-10-10 16:40:56 -0400

    structs

--- learning-go/main.go
+++ learning-go/main.go
@@ -1,19 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+	// note: capitalizing varibable/function allows it to be visible to outside of the "main" package
+}
 
 func main() {
-	fmt.Println("Hello world!")
-	var whatToSay string
-	var i int = 8
-	whatToSay = "Goodbye world"
-	fmt.Println(whatToSay)
-	fmt.Println("i is set to", i)
-
-	whatWasSaid, whenWasSaid := saySomething()
-	fmt.Println("The function returned", whatWasSaid, whenWasSaid)
+	user := User{
+		FirstName:   "Saul",
+		LastName:    "Goodman",
+		PhoneNumber: "416-614-4616",
+	}
+	log.Println(user.FirstName, user.LastName, "BirthDate", user.BirthDate)
 }
 
-func saySomething() (string, string) {
-	return "I am saying something", "now"
-}
+// func saySomething(s3 string) (string, string) {
+// 	log.Println("s from saySomething func is", s)
+// 	return s3, "world"
+// }
