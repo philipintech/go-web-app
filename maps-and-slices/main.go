c63874f1f938bfd45e363b77afd6ed390e014002
Philip C <philipintech@gmail.com>
2022-10-29 15:56:16 -0400

    maps pt2

--- maps-and-slices/main.go
+++ maps-and-slices/main.go
@@ -2,13 +2,24 @@ package main
 
 import "log"
 
+type User struct {
+	FirstName string
+	LastName  string
+}
+
 func main() {
 
-	myMap := make(map[string]int)
+	myMap := make(map[string]User)
 
-	myMap["dog"] = 1
-	myMap["other-dog"] = 2
-	myMap["dog"] = 3
-	log.Println(myMap["dog"])
-	log.Println(myMap["other-dog"])
+	me := User{
+		FirstName: "Philip",
+		LastName:  "Cheung",
+	}
+
+	myMap["me"] = me
+	log.Println(myMap["me"].FirstName, myMap["me"].LastName)
+
 }
+
+//Maps are immutable and are fast (performant)
+//Maps are programmatically built into the system not sorted (it does not stay in sorted way, must be accessed via key lookup myMap["myKey"])
